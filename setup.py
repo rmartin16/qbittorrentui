e447fb7c059dc6f0dc7a21e5d21a2418999ba3c7
russell <rmartin16@gmail.com>
2019-06-09 18:52:01 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='qbittorrentui',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={'': ['default.ini']},
     include_package_data=True,
